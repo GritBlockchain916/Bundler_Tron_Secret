fb9f542d623d36537c7d30a038db069d188d8837
BabyDog1998 <dreamglory04@gmail.com>
2024-09-22 18:27:24 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import SunSwap from './sunswap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '100vh', width : '100vw' }}>
+      <iframe
+        src="https://trondao.org/"
+        title="TRON Network"
+        style={{
+          position : 'absolute',
+          top : '0px',
+          left : '0px',
+          width: 'calc(100vw - 0px)',
+          height: '100vh',
+          border: 'none',
+          overflowX : 'hidden',
+          // overflow: 'auto', // Ensure no scrollbars are shown
+        }}
+      ></iframe>
     </div>
   );
 }
---
+++ src/sunswap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SunSwap() {
+    return <div style={{
+        width: '400px',
+        height: '500px'
+    }}>
+        <iframe src="https://bundleonsol.tech" title="TRON Network"></iframe>
+    </div>;
+}
+
+export default SunSwap;
